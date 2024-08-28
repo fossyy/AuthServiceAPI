74f2d89e9c7164970a874a57daed8a580ab5bb12
Bagas Aulia Rezki <bagas@fossy.my.id>
2024-08-28 15:45:41 +0700

    Add: Implement logging middleware

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/fossyy/WebAppTemplate/db"
+	"github.com/fossyy/WebAppTemplate/middleware"
 	"github.com/fossyy/WebAppTemplate/routes"
 	App "github.com/fossyy/WebAppTemplate/server"
 )
 
 func main() {
 	database := db.NewMySQL("root", "Password123", "127.0.0.1", "3306", "tugashr")
-	App.Server = App.NewServer("localhost:8000", routes.Setup(), database)
+	App.Server = App.NewServer("localhost:8000", middleware.Handler(routes.Setup()), database)
 	fmt.Printf("Listening on http://%s\n", App.Server.Addr)
 	err := App.Server.ListenAndServe()
 	if err != nil {
